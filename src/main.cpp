64167a68ed7fea66bb521f7e1a8ab930e4b689ef
Pranav Pesaladinne <pranavreddyp16@gmail.com>
2025-03-12 02:31:38 -0400

    Added test for demo

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,14 @@ int main(int argc, char* argv[]) {
     cout<<"Now we will get address 500 evicted"<<endl;
     cout<<"We will read from 4 more addresses (564, 628, 692, 756) that will clash with 500 and get it evicted"<<endl;
     cache.read_data(564);
+    cache.write_data(564, int_to_mem_dtype(10));
     cache.read_data(628);
+    cache.write_data(628, int_to_mem_dtype(11));
     cache.read_data(692);
+    cache.write_data(692, int_to_mem_dtype(12));
     cache.read_data(756);
+    cache.write_data(756, int_to_mem_dtype(13));
+    cout<<"Now let's take a look at the full cache"<<endl;
+    cache.show_cache();
+    cout<<"Now, since the cache line was evicted and it had the dirty bit set, the memory at address 500 has been written and is: "<<mem_dtype_to_int(mem.read_data(500))<<endl;
 }
