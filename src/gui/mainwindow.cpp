f998011b113807fe9c95d7d32f9657074b600f90
sagnikpal2004 <sagnikpal@umass.edu>
2025-04-04 00:42:11 -0400

    more UI

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include <QMenuBar>
 #include <QMessageBox>
+#include <QVBoxLayout>
+#include <QTableWidget>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     // Set up the main window
@@ -21,6 +24,33 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     connect(aboutAction, &QAction::triggered, [this]() {
         QMessageBox::information(this, "About", "ISA Simulator\nVersion 1.0");
     });
+
+    QWidget *centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+    centralWidget->setLayout(layout);
+
+    QTableWidget *instructionTable = new QTableWidget(this);
+    instructionTable->setColumnCount(3);
+    instructionTable->setHorizontalHeaderLabels({"Address", "Instruction", "Pipeline Stage"});
+    layout->addWidget(instructionTable);
+
+    QTableWidget *memoryTable = new QTableWidget(this);
+    memoryTable->setColumnCount(2);
+    memoryTable->setHorizontalHeaderLabels({"Address", "Data"});
+    layout->addWidget(memoryTable);
+
+    QTableWidget *cacheTable = new QTableWidget(this);
+    cacheTable->setColumnCount(3);
+    cacheTable->setHorizontalHeaderLabels({"Cache Address", "Data", "Status"});
+    layout->addWidget(cacheTable);
+
+    QPushButton *tickButton = new QPushButton("Tick", this);
+    connect(tickButton, &QPushButton::clicked, [this]() {
+        // Placeholder for tick functionality
+        QMessageBox::information(this, "Tick", "Tick button clicked. Implement the tick logic here.");
+    });
+    layout->addWidget(tickButton);
 }
 
 MainWindow::~MainWindow() {}
